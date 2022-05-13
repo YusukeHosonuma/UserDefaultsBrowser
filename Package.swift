807e454d405204384554233588868c7927f42ccf
Yusuke Hosonuma <tobi462@gmail.com>
2022-05-13 12:11:02 +0900

    change: use SwiftUI-Common as version instead of branch

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/YusukeHosonuma/SwiftPrettyPrint.git", from: "1.3.0"),
         .package(url: "https://github.com/pointfreeco/swift-case-paths.git", from: "0.8.0"),
-        .package(url: "https://github.com/YusukeHosonuma/SwiftUI-Common.git", branch: "main"),
+        .package(url: "https://github.com/YusukeHosonuma/SwiftUI-Common.git", from: "1.0.0"),
     ],
     targets: [
         .target(name: "UserDefaultsBrowser", dependencies: [
